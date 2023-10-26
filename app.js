a773aa7efe8f227722186514889e5784fa25f201
gliberov <124058342+gliberov@users.noreply.github.com>
2023-10-25 22:23:09 -0400

    yan helped

--- app.js
+++ app.js
@@ -15,31 +15,33 @@ DOMSelectors.name.addEventListener('submit' , function () {
     DOMSelectors
 }) */
 
-
-
-const DOMSelectors = {
-    button: document.getElementById('btn'),
-    input: document.querySelector("#input"),
-    text: document.querySelector("#text"),
-    box: document.getElementById("container-box"),
-    points: document.querySelectorAll(".point")
-};
-DOMSelectors.button.addEventListener('submit', function(event) {
-    event.preventDefault()
-    create()
-})
-
-function create()
-
-function backgroundAndText(background, text){
-    background.style.backgroundColor = 'red';
-    text.innerHTML = "This is now a big red box";
-}
 /* const cat = "meow";
 DOMSelectors.box.insertAdjacentHTML(`beforeend",'<h1>We are an ${cat}</h1>`); */
 
-DOMSelectors.button.addEventListener("click", function() {
+
+/* DOMSelectors.button.addEventListener("click", function() {
     let input = DOMSelectors.input.value; 
     DOMSelectors.box.insertAdjacentHTML("beforeend",`<p>${input}</p>`);
     DOMSelectors.input.value = "";
-});
+}); */
+
+const DOMSelectors = {
+    form: document.querySelector("#form"),
+    name: document.querySelector("#name"),
+    map: document.querySelector("#map"),
+    box: document.querySelector('#countries')
+};
+
+function create(){
+
+    DOMSelectors.box.insertAdjacentHTML("beforeend",`<div class="card">
+    <h2>${DOMSelectors.name.value}</h2>
+    <img src="${DOMSelectors.map.value}">
+    </div>`,);
+    }
+
+DOMSelectors.form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    create();
+})
+
